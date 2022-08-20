05a004706f2eec7c1cf61c3ea6c3204a470597b3
Teja8891 <102683189+Teja8891@users.noreply.github.com>
2022-08-20 22:29:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  const handleInput = (event) =>{
+   // use console.log
+    console.log(event.target.value);
+  }
+
+  // do not change id of input elements
+  return (
+    <div id="main">
+      <label htmlFor='text-input'>Text Input:- </label>
+      <input id="text-input" type={'text'} onInput={handleInput} />
+
+      <br/>
+      <br/>
+
+      <label htmlFor='num-input'>Number input</label>
+      <input id="num-input"  type={'number'} onInput={handleInput} />
+      <br/>
+    </div>
+  )
+}
+
+
+export default App;
